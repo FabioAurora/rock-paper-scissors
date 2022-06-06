3c9984a98ede914a4ecf8e2b7f248f99e4a5cda8
Fabio Aurora <fabioauroradev@gmail.com>
2022-06-06 11:38:01 +0100

    IMplemented the DOM and created a simple UI

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,59 @@
 "use strict";
 
+/* implementing the DOM */
+/* Accessing the nodes */
+const buttons = document.querySelectorAll("button[id]");
+const display = document.querySelector("#display");
+const playerPoints = document.querySelector("#playerPoints");
+const computerPoints = document.querySelector("#computerPoints");
+const restart = document.querySelector('#restart');
+
+/* creating an element for the display container */
+const result = document.createElement("p");
+
 let playerScore = 0;
 let computerScore = 0;
+
+/* iterating through all btn to retrieve the ID and use it for the conditional */
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const playerChoice = e.target.id;
+
+    /* function to play a single round */
+    function playRound(playerSelection, computerSelection) {
+      computerSelection = computerPlay();
+      playerSelection = playerChoice;
+
+      /* if/else condition to return a string with the winner of the round */
+      if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+      ) {
+        playerScore++;
+        result.textContent =`You won this round!!! ${playerSelection} beats ${computerSelection}`;
+      } else if (playerSelection === computerSelection) {
+        result.textContent = `Both played ${playerSelection}, it is a tie`;
+      } else {
+        computerScore++;
+        result.textContent = `You lost this round!!! ${computerSelection} beats ${playerSelection}`;
+      }
+      return display.appendChild(result)
+    }
+
+    /* stops the game is any player reaches 5 points */
+    if (playerScore === 5) {
+      return result.textContent = `You won the Game!!! with ${playerScore} vs ${computerScore}`;
+    }else if (computerScore === 5) {
+      return result.textContent = `computer won the Game!!! with ${computerScore} vs ${playerScore}`;
+    }
+
+    playRound();
+
+    playerPoints.textContent = playerScore;
+    computerPoints.textContent = computerScore;
+  });
+});
 
 /* function to make the computer's play */
 function computerPlay() {
@@ -10,43 +62,10 @@ function computerPlay() {
   return computerPlay[randomIndex];
 }
 
-/* function to play a single round */
-function playRound(playerSelection, computerSelection) {
-  computerSelection = computerPlay();
-  playerSelection = prompt(`please choose rock, paper or scissors`);
-  playerSelection = playerSelection.toLowerCase();
-
-  /* if/else condition to return a string with the winner of the round */
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    console.log(`You win!!! ${playerSelection} beats ${computerSelection}`);
-    return playerScore++
-  } else if (playerSelection === computerSelection) {
-    console.log(`It is a tie`);
-  } else {
-    console.log(`You lose!!! ${computerSelection} beats ${playerSelection}`);
-    return computerScore++
-  }
-}
-
-/* function to play 5 rounds */
-function game() {
-  let round = 0;
-
-  for (let i = 0; i < 5 ; i++) {
-      playRound();
-    round = i;
-  }
-
-  if (playerScore > computerScore) {
-    console.log(`Player win, with ${playerScore} point's`);
-    console.log(`computer lost, with ${computerScore} point's`);
-  }else if (playerScore === computerScore) {
-    console.log(`It is a tie, no one wins`);
-  } else console.log(`Player lost the game`)
-}
-
-game();
+restart.addEventListener('click', () => {
+  playerScore = 0;
+  computerScore = 0;
+  playerPoints.textContent = `0`;
+  computerPoints.textContent = `0`;
+  result.textContent = '';
+})
